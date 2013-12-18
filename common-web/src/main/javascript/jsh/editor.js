168bfa6b83a1a9d7562c28b424b3b3d86a0358be
Wiley <wiley.fuller@gmail.com>
2013-12-18 22:48:17 +1100

    moved editor code from main.js to editor.js

--- common-web/src/main/javascript/jsh/editor.js
+++ common-web/src/main/javascript/jsh/editor.js
@@ -1,6 +1,15 @@
 goog.provide('jsh.HackEditor');
 
+goog.require('goog.dom.ViewportSizeMonitor');
 goog.require('goog.ui.Component');
+goog.require('goog.ui.MenuItem');
+goog.require('goog.ui.SplitPane');
+goog.require('goog.ui.Toolbar');
+goog.require('goog.ui.ToolbarButton');
+goog.require('goog.ui.tree.TreeControl');
+goog.require('jsh.AceEditor');
+goog.require('jsh.HackList');
+goog.require('jsh.SplitPane');
 
 
 
@@ -11,7 +20,107 @@ goog.require('goog.ui.Component');
  * @extends {goog.ui.Component}
  * @constructor
  */
-jsh.SplitPane = function(opt_domHelper) {
+jsh.HackEditor = function(opt_domHelper) {
   goog.base(this, opt_domHelper);
+
+  this.outersplitpane_ = null;
+  this.innersplitpane_ = null;
+
+  this.viewSizeMonitor_ = new goog.dom.ViewportSizeMonitor();
+
+  //TODO This needs to go somewhere else, like a constant or something
+  this.splitPaneHandleWidth_ = 5;
 };
 goog.inherits(jsh.HackEditor, goog.ui.Component);
+
+
+/**
+ * Decorates an existing HTML DIV element as a SampleComponent.
+ *
+ * @param {Element} element The DIV element to decorate. The element's
+ *    text, if any will be used as the component's label.
+ * @override
+ */
+jsh.HackEditor.prototype.decorateInternal = function(element) {
+
+  goog.base(this, 'decorateInternal', element);
+
+  var toolbar = new goog.ui.Toolbar();
+
+  var btnSave = goog.dom.createDom('div');
+  btnSave.innerHTML = "<i class='fa fa-floppy-o icon'></i>" +
+      "<span class='toolbar-text'>Save</span>";
+  var btnSave = new goog.ui.ToolbarButton(btnSave);
+  toolbar.addChild(btnSave, true);
+
+  var btnClose = goog.dom.createDom('div');
+  btnClose.innerHTML = "<i class='fa fa-power-off icon'></i>" +
+      "<span class='toolbar-text'>Close</span>";
+  var btnSave = new goog.ui.ToolbarButton(btnClose);
+  toolbar.addChild(btnSave, true);
+
+  this.addChild(toolbar, true);
+
+  this.lhs = new goog.ui.Component();
+  this.editor = new jsh.AceEditor();
+  this.props = new goog.ui.Component();
+
+
+  this.innersplitpane_ = new jsh.SplitPane(this.editor, this.props,
+      goog.ui.SplitPane.Orientation.VERTICAL);
+  this.innersplitpane_.setInitialSize(300);
+  this.innersplitpane_.setHandleSize(this.splitPaneHandleWidth_);
+  this.innersplitpane_.setSecondComponentStatic(true);
+
+  this.outersplitpane_ = new jsh.SplitPane(this.lhs, this.innersplitpane_,
+      goog.ui.SplitPane.Orientation.HORIZONTAL);
+  this.outersplitpane_.setInitialSize(300);
+  this.outersplitpane_.setHandleSize(this.splitPaneHandleWidth_);
+
+  this.addChild(this.outersplitpane_, true);
+};
+
+
+/**
+ * Called when component's element is known to be in the document.
+ * @override
+ */
+jsh.HackEditor.prototype.enterDocument = function() {
+  goog.base(this, 'enterDocument');
+  this.resizeOuterSplitPane_();
+  goog.events.listen(this.viewSizeMonitor_,
+      goog.events.EventType.RESIZE, this.resizeOuterSplitPane_, false, this);
+  goog.events.listen(this.outersplitpane_, goog.ui.Component.EventType.CHANGE,
+      this.resizeInnerSplitPane_, false, this);
+  goog.events.listen(this.innersplitpane_, goog.ui.Component.EventType.CHANGE,
+      goog.events.Event.stopPropagation, false, this);
+
+};
+
+
+/**
+ * Handler for the viewSizeMonitor event, to resize the innerSplitPane
+ * @private
+ */
+jsh.HackEditor.prototype.resizeInnerSplitPane_ = function() {
+  var rhsheight = this.viewSizeMonitor_.getSize().height -
+      goog.style.getPosition(this.outersplitpane_.getElement()).y;
+  var rhswidth = this.viewSizeMonitor_.getSize().width -
+      this.outersplitpane_.getFirstComponentSize() - this.splitPaneHandleWidth_;
+  this.innersplitpane_.setSize(new goog.math.Size(rhswidth, rhsheight));
+  this.editor.resize();
+};
+
+
+/**
+ * Handler for the viewSizeMonitor event, to resize the outerSplitPane
+ * @private
+ */
+jsh.HackEditor.prototype.resizeOuterSplitPane_ = function() {
+  var lhsheight = this.viewSizeMonitor_.getSize().height -
+      goog.style.getPosition(this.outersplitpane_.getElement()).y;
+  var lhswidth = this.viewSizeMonitor_.getSize().width;
+  this.outersplitpane_.setSize(new goog.math.Size(lhswidth, lhsheight));
+  this.resizeInnerSplitPane_();
+};
+
--- common-web/src/main/javascript/main.js
+++ common-web/src/main/javascript/main.js
@@ -1,12 +1,4 @@
-goog.require('goog.dom.ViewportSizeMonitor');
-goog.require('goog.ui.MenuItem');
-goog.require('goog.ui.SplitPane');
-goog.require('goog.ui.Toolbar');
-goog.require('goog.ui.ToolbarButton');
-goog.require('goog.ui.tree.TreeControl');
-goog.require('jsh.AceEditor');
-goog.require('jsh.HackList');
-goog.require('jsh.SplitPane');
+goog.require('jsh.HackEditor');
 
 
 var splitPaneHandleWidth = 5;
@@ -17,71 +9,8 @@ goog.events.listenOnce(window, goog.events.EventType.LOAD, function() {
   var mainEl = goog.dom.createDom('div', 'ide');
   goog.dom.appendChild(document.body, mainEl);
 
-  var rootComponent = new goog.ui.Component();
 
-  var toolbar = new goog.ui.Toolbar();
+  var editor = new jsh.HackEditor();
+  editor.decorate(mainEl);
 
-  var btnSave = goog.dom.createDom('div');
-  btnSave.innerHTML = "<i class='fa fa-floppy-o icon'></i>" +
-      "<span class='toolbar-text'>Save</span>";
-  var btnSave = new goog.ui.ToolbarButton(btnSave);
-  toolbar.addChild(btnSave, true);
-
-  var btnClose = goog.dom.createDom('div');
-  btnClose.innerHTML = "<i class='fa fa-power-off icon'></i>" +
-      "<span class='toolbar-text'>Close</span>";
-  var btnSave = new goog.ui.ToolbarButton(btnClose);
-  toolbar.addChild(btnSave, true);
-
-
-  var lhs = new goog.ui.Component();
-  var editor = new jsh.AceEditor();
-  var props = new goog.ui.Component();
-
-
-  var innersplitpane = new jsh.SplitPane(editor, props,
-      goog.ui.SplitPane.Orientation.VERTICAL);
-  innersplitpane.setInitialSize(300);
-  innersplitpane.setHandleSize(splitPaneHandleWidth);
-  innersplitpane.setSecondComponentStatic(true);
-
-  var outersplitpane = new jsh.SplitPane(lhs, innersplitpane,
-      goog.ui.SplitPane.Orientation.HORIZONTAL);
-  outersplitpane.setInitialSize(300);
-  outersplitpane.setHandleSize(splitPaneHandleWidth);
-
-  rootComponent.addChild(toolbar, true);
-  rootComponent.addChild(outersplitpane, true);
-
-  rootComponent.render(mainEl);
-
-  innersplitpane.getElement().id = 'innersplitpane';
-  outersplitpane.getElement().id = 'outersplitpane';
-
-  lhs.addChild(new jsh.HackList(), true);
-
-
-  var viewSizeMonitor = new goog.dom.ViewportSizeMonitor();
-  var resizeInner = function() {
-    var rhsheight = viewSizeMonitor.getSize().height -
-        goog.style.getPosition(outersplitpane.getElement()).y;
-    var rhswidth = viewSizeMonitor.getSize().width -
-        outersplitpane.getFirstComponentSize() - splitPaneHandleWidth;
-    innersplitpane.setSize(new goog.math.Size(rhswidth, rhsheight));
-    editor.resize();
-  };
-  var resizeOuter = function() {
-    var lhsheight = viewSizeMonitor.getSize().height -
-        goog.style.getPosition(outersplitpane.getElement()).y;
-    var lhswidth = viewSizeMonitor.getSize().width;
-    outersplitpane.setSize(new goog.math.Size(lhswidth, lhsheight));
-    resizeInner();
-  };
-  resizeOuter();
-  goog.events.listen(viewSizeMonitor,
-      goog.events.EventType.RESIZE, resizeOuter);
-  goog.events.listen(outersplitpane,
-      goog.ui.Component.EventType.CHANGE, resizeInner);
-  goog.events.listen(innersplitpane,
-      goog.ui.Component.EventType.CHANGE, goog.events.Event.stopPropagation);
 });
