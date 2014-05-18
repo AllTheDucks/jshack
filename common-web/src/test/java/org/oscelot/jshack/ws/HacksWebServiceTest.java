31696d6073e861037048355b8315ea00dc9d6cc2
Wiley <wiley.fuller@gmail.com>
2014-05-18 18:20:53 +1000

    Fixed a broken test.

--- common-web/src/test/java/org/oscelot/jshack/ws/HacksWebServiceTest.java
+++ common-web/src/test/java/org/oscelot/jshack/ws/HacksWebServiceTest.java
@@ -27,55 +27,7 @@ public class HacksWebServiceTest {
         hackWS = new HacksWebService();
 
         hackWS.setHackManager(hackManager);
-        hackWS.setHackResourceService(resourceService);
 
-    }
-
-    @Test
-    public void createHack_withOutResources_savesSuccessfully() {
-        Hack hack = new Hack();
-
-        hackWS.createHack(hack);
-
-        verify(hackManager).persistHack(hack);
-        verify(resourceService, never()).persistResource(any(HackResource.class));
-    }
-
-
-    @Test
-    public void createHack_withOneResource_savesSuccessfully() {
-        Hack hack = new Hack();
-        List<HackResource> resources = new ArrayList<HackResource>();
-        HackResource resource = new HackResource();
-        resources.add(resource);
-        hack.setResources(resources);
-
-        hackWS.createHack(hack);
-
-        verify(hackManager).persistHack(hack);
-        verify(resourceService).persistResource(resource);
-        verifyNoMoreInteractions(resourceService);
-    }
-
-    @Test
-    public void createHack_withMultipleResources_savesSuccessfully() {
-        Hack hack = new Hack();
-        List<HackResource> resources = new ArrayList<HackResource>();
-        HackResource resource1 = new HackResource();
-        HackResource resource2 = new HackResource();
-        HackResource resource3 = new HackResource();
-        resources.add(resource1);
-        resources.add(resource2);
-        resources.add(resource3);
-        hack.setResources(resources);
-
-        hackWS.createHack(hack);
-
-        verify(hackManager).persistHack(hack);
-        verify(resourceService).persistResource(resource1);
-        verify(resourceService).persistResource(resource2);
-        verify(resourceService).persistResource(resource3);
-        verifyNoMoreInteractions(resourceService);
     }
 
 }
