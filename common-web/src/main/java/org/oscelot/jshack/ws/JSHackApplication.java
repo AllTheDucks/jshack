6a8c80d1da738d20d8549cfd27a1a96b5aec72c1
wiley <wiley@alltheducks.com>
2015-11-09 12:17:38 +1000

    fixed jersey so that it compiles and deploys, and starts Application. Doesn't seem to be accessible via URL yet though.

--- common-web/src/main/java/org/oscelot/jshack/ws/JSHackApplication.java
+++ common-web/src/main/java/org/oscelot/jshack/ws/JSHackApplication.java
@@ -3,8 +3,8 @@ package org.oscelot.jshack.ws;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.jvnet.hk2.spring.bridge.api.SpringBridge;
-import org.jvnet.hk2.spring.bridge.api.SpringIntoHK2Bridge;
+//import org.jvnet.hk2.spring.bridge.api.SpringBridge;
+//import org.jvnet.hk2.spring.bridge.api.SpringIntoHK2Bridge;
 import org.oscelot.jshack.security.JSHackAuthFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,19 +16,15 @@ import javax.servlet.ServletContext;
 import javax.ws.rs.ApplicationPath;
 
 
-@ApplicationPath("resources")
+@ApplicationPath("ws")
 public class JSHackApplication extends ResourceConfig {
     final Logger logger = LoggerFactory.getLogger(JSHackApplication.class);
 
     @Inject
     public JSHackApplication(ServiceLocator serviceLocator, ServletContext servletContext) {
 
-        // Register a Spring to HK2 Bridge so we can use our Spring beans in Jersey land.
-        SpringBridge.getSpringBridge().initializeSpringBridge(serviceLocator);
+//        System.out.println("Hello service locator: " + serviceLocator);
         final WebApplicationContext springContext = WebApplicationContextUtils.getWebApplicationContext(servletContext);
-        SpringIntoHK2Bridge springBridge = serviceLocator.getService(SpringIntoHK2Bridge.class);
-        springBridge.bridgeSpringBeanFactory(springContext);
-
 
         packages("org.oscelot.jshack.ws;org.oscelot.jshack.security");
         register(JacksonJsonProvider.class);
